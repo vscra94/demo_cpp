0f56d0f6745f2029e2dece0591c98bf0de0c5016
Vikas kumar SHARMA <vscra94@gmail.com>
2023-03-29 12:13:29 +0200

    ADD testscript.sh

---
+++ testscript.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "this script is from github"
+NUMBER=$(($NUMBERONE + $NUMBERTWO))
+echo "$NUMBER"
+echo "This was added"
+echo "This script is complete now."
+echo "Hello from vikas jenkins docker container"
